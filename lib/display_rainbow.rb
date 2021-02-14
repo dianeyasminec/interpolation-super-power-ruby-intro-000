86f9d1120b9bac511cf55b9b082cd48e71ba8679
Diane Compaore <dianeyasminec60@gmail.com>
2021-02-14 22:42:17 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,12 @@
 def display_rainbow(color_array)
 print "R: #{color_array[0]}"
+print "O: #{color_array[1]}"
+print "Y: #{color_array[2]}"
+print "G: #{color_array[3]}"
+print "B: #{color_array[4]}"
+print "I: #{color_array[5]}"
+print "V: #{color_array[6]}"
+
 end
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 
